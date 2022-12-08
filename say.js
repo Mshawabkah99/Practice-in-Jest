333f2954ed0808bd76231a8c7e4c774581889209
Mshawabkah99 <mohannadshawabkah@yahoo.com>
2022-12-08 16:34:39 +0300

    update

---
+++ say.js
@@ -0,0 +1,9 @@
+const thesum = (num1,num2)=>{
+    return num1+(num2||null) ;
+    }
+
+
+
+    module.exports=thesum;
+
+    // I am a developer
---
+++ say.test.js
@@ -0,0 +1,11 @@
+const say = require('./say')
+
+
+test ("check in this function",()=>{
+
+expect(say(500)).toBe(500)
+
+
+})
+
+// I am a QA 
